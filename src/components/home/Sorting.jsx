c89119fc85866fe5a838545d0d749a8cf43420bf
Ozer Onder <hozeronder@gmail.com>
2023-05-06 23:40:50 +0300

    select button fixed

--- src/components/home/Sorting.jsx
+++ src/components/home/Sorting.jsx
@@ -4,7 +4,7 @@ const Sorting = ({setSort}) => {
     return (
         <div className='bg-gray-100 my-5 p-5 flex items-center justify-end'>
             <select onChange={e => setSort(e.target.value)} className='bg-white-200 py-3 px-5' name="" id="">
-                <option disabled value="">Select</option>
+                <option disabled selected value="">Select</option>
                 <option value="inc">Increase</option>
                 <option value="dec">Decrease</option>
             </select>
