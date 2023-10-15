1ab4502aa51a196330301b56afca519286112500
hozeronder <hozeronder@gmail.com>
2023-10-16 02:59:30 +0300

    Hotel reservations app v0.66

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,21 +6,18 @@ const Footer = () => {
             <div className="col-span-8 sm:col-span-4">
                 <SimpleMap/>
             </div>
-            <div id="contact" className="grid grid-cols-8">
-                        <div className="col-span-8 sm:col-span-2">
-                            <div className="text-sm description adress">
-                                <span>9 Briarhill Lane</span><br/>
-                                <span>Youngstown</span><br/>
-                                <span>Ohio</span><br/>
+            <div id="contact" className="">
+                            <div className="text-sm adress">
+                                <span>9 Briarhill Lane</span>
+                                <span>Youngstown</span>
+                                <span>Ohio</span>
                                 <span>330-303-2758</span>
-                            </div>
                         </div>
 
-                        <div className="col-span-8 sm:col-span-2">
-                            <div className="text-sm description items-end ">
-                                <span>Contact & Directions</span><br/>
-                                <span>Privacy Policy</span><br/>
-                                <span>Digital Accessibility</span><br/>
+                        <div className="">
+                            <div className="text-sm footer-buttons">
+                                <span>Privacy Policy</span>
+                                <span>Digital Accessibility</span>
                                 <span>Site Credits</span>
                             </div>
                         </div>
